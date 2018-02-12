4df6e2fbb25524f76191cbc9ffcbb773daedb4d8
Ansi Britto <ansi@wegotpop.com>
2018-02-12 12:24:28 +0000

    First Commit git init
    :q
    exit
    
    exit()
    
    :
    
    q

---
+++ hello.py
@@ -0,0 +1,24 @@
+"""
+The world's most awesome hello world module
+"""
+import sys
+
+def hello(name='World'):
+  """
+  Return a greeting for the given name.
+  """
+  return 'Hello, {}'.format(name)
+
+def main():
+    """
+    Reads input from args passed into the script and prints the output to stout.
+    """
+    args=sys.argv[1:]
+    name = ' '.join(args).strip()
+    if name:
+        print(hello(name))
+    else:
+        print(hello())
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_hello.py
@@ -0,0 +1,19 @@
+"""
+Tests
+"""
+import pytest
+import hello
+# import builtins
+# from unittest import mock
+
+def test_hello_no_name():
+  """
+  Checks that if no name is passed, we get "Hello, World!"
+  """
+  assert hello.hello() == 'Hello, World'
+
+def test_hello_with_name():
+  """
+  If passed a name, we get "Hello, name"
+  """
+  assert hello.hello('Ansi') == 'Hello, Ansi'
